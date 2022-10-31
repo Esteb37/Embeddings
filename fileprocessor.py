4025612122c16f8e85ae773fb567d50985ae40a9
Esteb37 <esteban37padilla@gmail.com>
2022-10-30 18:00:35 -0600

    Increase chunkifying speed by precalculating size

--- fileprocessor.py
+++ fileprocessor.py
@@ -4,84 +4,93 @@ import os
 import inspect
 import time
 import linecache
+import h5py
+import math
+
 
 class FileProcessor:
-	
-	def __init__(self, file):
-		self.filename = file
-		self.cores = mp.cpu_count()
-  
-		self.files_dir = "files"
-		self.chunk_prefix = self.files_dir+"/chunk"
-		self.encoding = "utf8"
-		
-		self.chunkify()
-		
-	def chunkify(self):
-		files_dir = self.files_dir
-		cores = self.cores
-  
-		with open(self.filename, 'r', encoding=self.encoding) as input_file:
-  
-			output_files = []
 
-			if(not os.path.exists(files_dir)):
-				os.mkdir(files_dir)
-			
-			num_files = len([name for name in os.listdir(files_dir) if os.path.isfile(os.path.join(files_dir, name))])
-	
-			if(num_files != cores):
-				
-				print("Chunking file "+self.filename+" into "+str(cores)+" chunks. This will only happen once.")
-    
-				for i in range(0, cores):
-					output_files.append(open(self.chunk_prefix + str(i+1) + '.txt', 'w', encoding=self.encoding))
-		
-				for line in input_file:
-					output_files[hash(line) % cores].write(line)
-	
-				for i in range(0, cores):
-					output_files[i].close()
-    
-	def process_file(self, filename, index, shared_model):
-		
-		filename = self.chunk_prefix+str(index) + ".txt"
-	
-		for lineno,line in enumerate(open(filename, 'r', encoding=self.encoding)):
-			word_end = line.find(' ')
-			word = line[:word_end]
-			shared_model[word] = np.fromstring(line[word_end+1:], dtype=float, sep=' ')
+    def __init__(self, file, cores):
+        self.filename = file
+        self.cores = cores
 
-		print("Processed chunk #"+str(index))
-  
-		return shared_model
+        self.files_dir = "files"
+        self.chunk_prefix = self.files_dir+"/chunk"
+        self.encoding = "utf8"
 
+        self.chunkify()
 
-	def model_from_file(self):
-		
-		t0 = time.time()
-  
-		manager = mp.Manager()
-		
-		model = manager.dict()
-		
-		processes = list()
+    def chunkify(self):
+        files_dir = self.files_dir
+        cores = self.cores
+        filename = self.filename
+        chunk_prefix = self.chunk_prefix
+        encoding = self.encoding
 
-		"""
-		with open(f'./tmp_func.py', 'w') as file:
-			file.write(inspect.getsource(process_file).replace(process_file.__name__, "task"))
+        if not os.path.exists(files_dir):
+            os.makedirs(files_dir)
+        else:
+            if (len(os.listdir(files_dir)) == cores):
+                return
 
-		from tmp_func import task"""
+        start = time.time()
 
-		print("Processing file "+self.filename)
-  
-		with mp.Pool(processes=self.cores) as pool:
-			for i in range(self.cores):
-				processes.append(pool.apply_async(self.process_file, args=(self.filename, i+1, model)))
-	
-			for p in processes:
-				p.get()
+        print("Chunkifying file "+filename+" into " +
+              str(cores)+" chunks. This will only happen once.")
 
-		print("Finished processing file "+self.filename+" in "+str((time.time()-t0)/60)+" minutes")
-  
-		return model
+        filesize = os.path.getsize(filename)
+        first_line_size = len(linecache.getline(filename, 1))
+        num_lines = math.ceil(filesize/first_line_size)
+        lines_per_chunk = num_lines//cores
+
+        with open(filename, 'r', encoding=encoding) as file:
+
+            for i in range(cores - 1):
+
+                with open(chunk_prefix+str(i+1)+".txt", 'w', encoding=encoding) as chunk:
+
+                    for j in range(lines_per_chunk):
+                        line = file.readline()
+                        chunk.write(line)
+
+            with open(chunk_prefix+str(cores)+".txt", 'w', encoding=encoding) as chunk:
+                chunk.write(file.read())
+
+        print("Chunkifying finished in "+str(time.time()-start)+" seconds.")
+
+    def process_file(self, filename, index, shared_model):
+
+        filename = self.chunk_prefix+str(index) + ".txt"
+
+        for lineno, line in enumerate(open(filename, 'r', encoding=self.encoding)):
+            word_end = line.find(' ')
+            word = line[:word_end]
+            shared_model[word] = np.fromstring(
+                line[word_end+1:], dtype=float, sep=' ')
+
+        return shared_model
+
+    def model_from_file(self):
+
+        t0 = time.time()
+
+        manager = mp.Manager()
+
+        model = manager.dict()
+
+        processes = list()
+
+        print("Extracting model from files...")
+
+        with mp.Pool(processes=self.cores) as pool:
+            for i in range(self.cores):
+                processes.append(pool.apply_async(
+                    self.process_file, args=(self.filename, i+1, model)))
+
+            for p in processes:
+                p.get()
+
+        print("Finished extracting model from files in " +
+              str(time.time()-t0)+" seconds")
+
+        return model
--- glove.py
+++ glove.py
@@ -1,30 +1,32 @@
 import fileprocessor
 from multipledispatch import dispatch
+from multiprocessing import cpu_count
 import importlib
-import linecache 
+import linecache
 import numpy as np
 
+
 class Glove:
-    
-	def __init__(self, glove_file):
-		importlib.reload(fileprocessor)
-		self.filename = glove_file
-		self.file_processor = fileprocessor.FileProcessor(glove_file)
-		self.load_model()
-  
-	def load_model(self):
-		self.model = self.file_processor.model_from_file()
 
-	def get_word(self, word):
-		return self.model[word]
+    def __init__(self, glove_file, cores=cpu_count()):
+        importlib.reload(fileprocessor)
+        self.filename = glove_file
+        self.file_processor = fileprocessor.FileProcessor(glove_file, cores)
+        self.load_model()
 
-	def get_words(self, words):
-		return np.array([self.get_word(word) for word in words])
+    def load_model(self):
+        self.model = self.file_processor.model_from_file()
 
-	@dispatch(list)
-	def __call__(self, words):
-		return self.get_words(words)		
+    def get_word(self, word):
+        return self.model[word]
 
-	@dispatch(str)
-	def __call__(self, word):
-		return self.get_word(word)
+    def get_words(self, words):
+        return np.array([self.get_word(word) for word in words])
+
+    @dispatch(list)
+    def __call__(self, words):
+        return self.get_words(words)
+
+    @dispatch(str)
+    def __call__(self, word):
+        return self.get_word(word)
